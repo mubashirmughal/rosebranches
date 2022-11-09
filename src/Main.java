b9852b3b6a5cd68f7d3b41bd1c07a119f1831682
Muhammad Mubashir Mughal <donleo26@gmail.com>
2022-11-09 15:02:27 -0500

    variable and input

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a;
+        System.out.println("How much did you receive the money ($)? ");
+        a = sc.nextInt();
         
+
 
 
     }
