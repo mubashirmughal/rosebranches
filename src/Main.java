8232469680f0ec2bc0cbc2939000a1f67af152c9
Muhammad Mubashir Mughal <donleo26@gmail.com>
2022-11-09 20:32:14 -0500

    adsfa

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,22 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a;
-        System.out.println("How much did you receive the money ($)? ");
+        System.out.println("How much money did you receive ($)? ");
         a = sc.nextInt();
         int k = (int)(a*(75/100.0f));
 
         System.out.println("Book and Supplies: " +k);
 
+        int remainingMoney = a-k;
+        System.out.println("Remaining Money : " +remainingMoney);
+
+        int coffee = (remainingMoney / 3) / 2;
+        int fcomp = (remainingMoney / 3) / 4;
+        int subticket = (remainingMoney / 3) / 3;
+        double r = a * 0.005 ;
+
+       System.out.println("you can then buy:\n" + coffee + " coffees \n"  + fcomp + " Flash Computer Numbers\n" + subticket + " Subway Tickets\n" + r + " remaining ");
 
     }
 }
+
